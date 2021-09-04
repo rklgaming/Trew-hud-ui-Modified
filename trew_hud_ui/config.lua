fa085600f65cfcba8524c33ab029d798a51c6dba
rklgaming <90053019+rklgaming@users.noreply.github.com>
2021-09-04 17:15:29 +0800

    Add files via upload

---
+++ trew_hud_ui/config.lua
@@ -0,0 +1,69 @@
+Config = {}
+
+Config.Locale = 'en'
+
+Config.serverLogo = 'https://i.imgur.com/AcgDL9f.png'
+
+Config.font = {
+	name 	= 'Montserrat',
+	url 	= 'https://fonts.googleapis.com/css?family=Montserrat:300,400,700,900&display=swap'
+}
+
+Config.date = {
+	format	 	= 'default',
+	AmPm		= false
+}
+
+Config.voice = {
+
+	levels = {
+		default = 5.0,
+		shout = 12.0,
+		whisper = 1.0,
+		current = 0
+	},
+	
+	keys = {
+		distance 	= 'z',
+	}
+}
+
+
+Config.vehicle = {
+	speedUnit = 'KMH',
+	maxSpeed = 240,
+
+	keys = {
+		seatbelt 	= 'B',
+		key 	= 'LEFTCTRL',
+		cruiser		= 'LEFTSHIFT',
+		signalLeft	= 'LEFT',
+		signalRight	= 'RIGHT',
+		signalBoth	= 'DOWN',
+	}
+}
+
+Config.ui = {
+	showServerLogo		= true,
+
+	showJob		 		= false,
+
+	showWalletMoney 	= false,
+	showBankMoney 		= false,
+	showBlackMoney 		= false,
+	showSocietyMoney	= false,
+
+	showDate 			= false,
+	showLocation 		= false,
+	showVoice	 		= false,
+
+	showHealth			= true,
+	showArmor	 		= true,
+	showStamina	 		= true,
+	showHunger 			= true,
+	showThirst	 		= true,
+
+	showMinimap			= true,
+
+	showWeapons			= false,	
+}
